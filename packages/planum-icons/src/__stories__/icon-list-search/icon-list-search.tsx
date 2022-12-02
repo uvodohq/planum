801da48a2ab3017c668ee3c3326fcba5880e3849
Emin Qasimov <eminqasimov@outlook.com>
2022-12-02 13:59:13 +0400

    icons story duotone add

--- packages/planum-icons/src/__stories__/icon-list-search/icon-list-search.tsx
+++ packages/planum-icons/src/__stories__/icon-list-search/icon-list-search.tsx
@@ -17,11 +17,67 @@ const iconList: Item[] = Object.keys(icons).map((key) => {
   }
 })
 
+const duotoneList = [
+  'ChartBarIcon',
+  'CirclesFourIcon',
+  'ClipboardTextIcon',
+  'DoorIcon',
+  'GearIcon',
+  'HouseIcon',
+  'PaintBrushBroadIcon',
+  'PuzzlePieceIcon',
+  'StorefrontIcon',
+  'UserIcon',
+]
+
+export const allIconsCount = iconList.length
+export const duotoneIconsCount = duotoneList.length
+
+export function DuotoneIcons() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+        gap: 48,
+        padding: '0 32px',
+      }}>
+      {duotoneList.map((name) => {
+        // @ts-expect-error icon
+        const Icon = icons[name]
+
+        return (
+          <div
+            key={name}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 16,
+            }}>
+            <div>
+              <Icon weight="duotone" />
+            </div>
+            <div>{name}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
 function InputSearch(props: any) {
   return (
     <input
       placeholder="Search icon"
       onChange={(e) => props.onChange(e.target.value)}
+      style={{
+        height: 48,
+        padding: 12,
+        width: '100%',
+        borderRadius: 4,
+        border: '1px solid',
+      }}
     />
   )
 }
@@ -41,6 +97,7 @@ export const SearchIcons = () => {
 
   return (
     <div style={{ width: '100%' }}>
+      <h2>There are {allIconsCount} icons in planum icons</h2>
       <div
         style={{
           maxWidth: '400px',
