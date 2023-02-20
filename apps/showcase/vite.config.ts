8a053f17f5d664132cf4274b2490c09a49d12d33
Emin Qasimov <eminqasimov@outlook.com>
2023-02-20 12:33:47 +0400

    showcase vite update v4, libs splitted

--- apps/showcase/vite.config.ts
+++ apps/showcase/vite.config.ts
@@ -5,4 +5,16 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-with-dom': ['react', 'react-dom'],
+          'planum-core-framer': ['@uvodohq/planum', 'framer-motion'],
+          'planum-phone': ['@uvodohq/planum-phone'],
+          'planum-editor': ['@uvodohq/planum-editor'],
+        },
+      },
+    },
+  },
 })
