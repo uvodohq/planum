dd02bfc1434a6ce446ee1c34968d65fdf8f9dc65
Nijat Bakhtiyar <nijatbakhtiyar@gmail.com>
2023-06-19 00:52:10 +0400

    image resize

--- apps/showcase/src/pages/container/editor.tsx
+++ apps/showcase/src/pages/container/editor.tsx
@@ -1,7 +1,8 @@
-import { Flex } from '@uvodohq/planum'
+import { Flex, useModalState } from '@uvodohq/planum'
 import { Editor } from '@uvodohq/planum-editor'
 
 export default function ExtraContainer() {
+  const modalState = useModalState()
   return (
     <>
       <Flex css={{ gap: 32, py: 64, maxWidth: 608 }}>
@@ -9,6 +10,28 @@ export default function ExtraContainer() {
           label={'Controlled'}
           aria-label="Description"
           placeholder="Product description (optional)"
+          imageModal={modalState}
+          onChange={(value) => console.warn('valueeee', value)}
+          imageUpload={(editor) => {
+            // editor.chain().focus().to
+            editor
+              .chain()
+              .focus()
+              .setImage({
+                src: 'https://images.pexels.com/photos/6054896/pexels-photo-6054896.jpeg',
+                title: 'sadas',
+                style: {
+                  width: '100px',
+                  height: '200px',
+                },
+              })
+              .run()
+            //https://dev.to/brampayrequest/tiptap-image-resize-extension-2328
+            //images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2
+            console.warn('editor', {
+              editor,
+            })
+          }}
         />
       </Flex>
       <Flex css={{ gap: 32, pb: 64, maxWidth: 608 }}>
