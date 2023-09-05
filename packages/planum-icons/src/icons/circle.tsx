bbc5dddbba6cd3de0d5d7ec5e6ca64255ff28dd2
Nijat Bakhtiyar <nijatbakhtiyar@Nijats-MacBook-Air.local>
2023-09-05 18:37:58 +0400

    add circle icon

---
+++ packages/planum-icons/src/icons/circle.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from 'react'
+
+import type { IconProps, IconWeightComponent } from '../icon'
+import { Icon } from '../icon'
+
+const Regular: IconWeightComponent = ({ color }) => (
+  <>
+    <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Z" />
+  </>
+)
+
+const weights = {
+  // duotone: Duotone,
+  regular: Regular,
+}
+
+const CircleIcon = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
+  <Icon ref={ref} alt="Circle Icon" {...props} weights={weights} />
+))
+
+CircleIcon.displayName = 'CircleIcon'
+
+export default CircleIcon
